4cce2045b8dfadab73e46e411c89456d173d869d
Wael Amri <wael.amri.97.05@gmail.com>
2025-04-15 21:34:38 +0000

    add posted_at column in model to help partition the table in bq

--- models/analytics/nps_feedback.sql
+++ models/analytics/nps_feedback.sql
@@ -1,11 +1,15 @@
 -- models/analytics/nps_feedback.sql
 
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    partition_by={"field": "posted_at", "data_type": "date"}
+) }}
 
 SELECT
     customer_id,
     nps_score,
-    nps_comment
+    nps_comment,
+    posted_at
 FROM
     {{ ref('nps_data') }}
 
